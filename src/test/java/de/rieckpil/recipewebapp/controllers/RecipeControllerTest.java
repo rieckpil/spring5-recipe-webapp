18db937db11c354ec414d76aaad0d111fe56c8d7
Philip Riecks <philip@riecks.io>
2017-12-17 19:43:30 +0100

    fix broken test

--- src/test/java/de/rieckpil/recipewebapp/controllers/RecipeControllerTest.java
+++ src/test/java/de/rieckpil/recipewebapp/controllers/RecipeControllerTest.java
@@ -40,7 +40,7 @@ public class RecipeControllerTest {
 
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(cut).build();
 
-        mockMvc.perform(get("/recipe/show/1")).andExpect(status().isOk()).andExpect(view().name("recipe/show")).andExpect(model().attributeExists("recipe"));
+        mockMvc.perform(get("/recipe/1/show")).andExpect(status().isOk()).andExpect(view().name("recipe/show")).andExpect(model().attributeExists("recipe"));
 
     }
 
